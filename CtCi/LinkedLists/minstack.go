68c9ae7cd32760896688415e96438d04cff74f7d
BartekTao <pony830906@gmail.com>
2022-02-02 10:22:40 +0800

    MinStack

--- CtCi/LinkedLists/minstack.go
+++ CtCi/LinkedLists/minstack.go
@@ -2,6 +2,8 @@ package ctci
 
 import "math"
 
+// minStack 的空間使用可以優化
+
 type MinStack struct {
 	stack    []int
 	minStack []int
@@ -19,15 +21,16 @@ func (this *MinStack) Push(x int) {
 }
 
 func (this *MinStack) Pop() {
-
+	this.stack = this.stack[:len(this.stack)-1]
+	this.minStack = this.minStack[:len(this.minStack)-1]
 }
 
 func (this *MinStack) Top() int {
-
+	return this.stack[len(this.stack)-1]
 }
 
 func (this *MinStack) GetMin() int {
-
+	return this.minStack[len(this.minStack)-1]
 }
 
 func min(x, y int) int {
