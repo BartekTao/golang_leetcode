73160e07f3ac1ac3cc427bb016f5ddc5339ca3af
BartekTao <pony830906@gmail.com>
2022-07-02 15:46:00 +0800

    Valid Parentheses

---
+++ leetcode/valid_parentheses.go
@@ -0,0 +1,24 @@
+package leetcode
+
+func isValid(s string) bool {
+	stack := []byte{}
+	pairs := map[byte]byte{
+		'(': ')',
+		'[': ']',
+		'{': '}',
+	}
+	for i := 0; i < len(s); i++ {
+		v := s[i]
+		if _, ok := pairs[s[i]]; ok {
+			stack = append(stack, v)
+		} else {
+			// pop
+			if len(stack) == 0 || pairs[stack[len(stack)-1]] != v {
+				return false
+			} else {
+				stack = stack[:len(s)-1]
+			}
+		}
+	}
+	return true
+}
